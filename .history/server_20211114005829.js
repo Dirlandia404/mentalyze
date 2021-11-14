102b1e9c76908a2b776ca0473359538dc1ac5393
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2021-11-14 09:41:11 -0300

    chat em js

---
+++ .history/server_20211114005829.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ajs").renderFile);
+app.set("view engine", "html");
+
+app.use("/chat", (req, res) => {
+  res.render("chat.html");
+});
+
+io.on("connection", (socket) => {
+  console.log(`Socket conectado`);
+});
+server.listen(3000);
